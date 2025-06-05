5b83213b9fd83c588474b0542c93d4c6eed753b9
Eric Spencer <espencer2@luc.edu>
2025-06-05 18:50:38 -0500

    Remove unused classes: ComplexProgram, DeadlockExample, DiningPhilosophers, and MySimpleProgram to streamline the project and eliminate redundant code.

--- tlajvm/src/main/java/com/tlajvm/parser/TestProgram.java
+++ tlajvm/src/main/java/com/tlajvm/parser/TestProgram.java
@@ -1,14 +1,16 @@
 package com.tlajvm.parser;
 
-public class ComplexProgram {
-    private int counter;
-    private boolean isRunning;
-    
-    public ComplexProgram() {
-        this.counter = 0;
-        this.isRunning = true;
+public class TestProgram {
+    private int counter = 0;
+    private boolean isRunning = true;
+    private int[] values = new int[3];
+
+    public TestProgram() {
+        for (int i = 0; i < values.length; i++) {
+            values[i] = i;
+        }
     }
-    
+
     public void process() {
         while (isRunning && counter < 5) {
             if (counter % 2 == 0) {
@@ -19,25 +21,25 @@ public class ComplexProgram {
             checkStatus();
         }
     }
-    
+
     private void incrementCounter() {
         counter++;
         System.out.println("Counter incremented: " + counter);
     }
-    
+
     private void decrementCounter() {
         counter--;
         System.out.println("Counter decremented: " + counter);
     }
-    
+
     private void checkStatus() {
         if (counter >= 5) {
             isRunning = false;
         }
     }
-    
+
     public static void main(String[] args) {
-        ComplexProgram program = new ComplexProgram();
+        TestProgram program = new TestProgram();
         program.process();
     }
 } 
